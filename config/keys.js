37456bb602415f0b0ef1769781ed7cdfe3bcdb16
Beta (Ben) Anggoro <ben@benworkandplay.com>
2018-06-05 19:20:34 +0700

    Created JWT Token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://benbkk:b3nbkk@ds141870.mlab.com:41870/devconnect'
+    mongoURI: 'mongodb://benbkk:b3nbkk@ds141870.mlab.com:41870/devconnect',
+    secretOrKey: 'secret'
 }
